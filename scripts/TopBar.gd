19fad535f2b837ee78a39ce62afb46650bdcbabd
Loki Chow <lohchness@gmail.com>
2025-07-29 23:39:38 +1000

    refactor: topbar inherits twostateswitch

--- scripts/TopBar.gd
+++ scripts/TopBar.gd
@@ -1,27 +1,10 @@
 class_name TopBar
-extends Node2D
+extends TwoStateSwitch
 
 var base_position: Vector2
 var off_screen: Vector2
 
 @onready var timer: CountdownBar = $Timer
-
-
-func _ready() -> void:
-	base_position = position
-	off_screen = base_position
-
-
-func _physics_process(delta: float) -> void:
-	position = lerp(position, off_screen, 20 * delta)
-
-
-func move_off_screen(offset: Vector2) -> void:
-	off_screen = base_position - offset
-
-
-func move_on_screen() -> void:
-	off_screen = base_position
 
 
 func pause_timer() -> void:
