20d9a72bdf6fcf861eafbfbf364b835389874070
Loki Chow <lohchness@gmail.com>
2025-07-30 10:40:14 +1000

    fix: rewrite switch function and add option to start off screen

--- scripts/utils/two_state_switch.gd
+++ scripts/utils/two_state_switch.gd
@@ -16,45 +16,52 @@ extends Node2D
 ## The offset vector to move this OFF screen
 @export var offset_vector: Vector2 = Vector2.ZERO
 ## Is the initial position ON screen?
-@export var start_on_screen: bool = false
+@export var on_screen_in_editor: bool = false
+## Enable ONLY IF on_screen_in_editor is true.
+@export var should_start_off_screen: bool = false
 
 var on_screen_position: Vector2
 var off_screen_position: Vector2
 var move_to: Vector2
-var is_on_screen: bool
+var at_target_position: bool
 
 
 func _ready() -> void:
-	is_on_screen = start_on_screen
-
-	if not use_editor_as_start:
-		position = target_position
-		if not start_on_screen:
-			position += offset_vector
-		on_screen_position = target_position
-	move_to = position
+	at_target_position = on_screen_in_editor
 
 	### FIXME Horrendous code
-	#if use_editor_as_start and start_on_screen:
+	#if use_editor_as_start and on_screen_in_editor:
+	#at_target_position = true
 	#on_screen_position = position
 	#off_screen_position = position + offset_vector
-	#elif not use_editor_as_start and start_on_screen:
+	#elif not use_editor_as_start and on_screen_in_editor:
+	#at_target_position = true
 	#on_screen_position = target_position
 	#off_screen_position = target_position + offset_vector
-
-	#elif use_editor_as_start and not start_on_screen:
+#
+	#elif use_editor_as_start and not on_screen_in_editor:
+	#at_target_position = false
 	#on_screen_position = position + offset_vector
 	#off_screen_position = position
-	#elif not use_editor_as_start and not start_on_screen:
+	#elif not use_editor_as_start and not on_screen_in_editor:
+	#at_target_position = false
 	#on_screen_position = target_position + offset_vector
 	#off_screen_position = target_position
 
-	on_screen_position = position if use_editor_as_start else target_position
-	off_screen_position = on_screen_position + offset_vector
-	if not start_on_screen:
-		var tmp: Vector2 = on_screen_position
-		on_screen_position = off_screen_position
-		off_screen_position = tmp
+	if on_screen_in_editor:
+		on_screen_position = position if use_editor_as_start else target_position
+		off_screen_position = on_screen_position + offset_vector
+		position = on_screen_position
+	elif not on_screen_in_editor:
+		off_screen_position = position if use_editor_as_start else target_position
+		on_screen_position = off_screen_position + offset_vector
+		position = off_screen_position
+
+	if on_screen_in_editor and should_start_off_screen:
+		at_target_position = false
+		position = off_screen_position
+
+	move_to = position
 
 
 func _physics_process(delta: float) -> void:
@@ -64,18 +71,17 @@ func _physics_process(delta: float) -> void:
 func move_on_screen() -> void:
 	set_process_input(true)
 	move_to = on_screen_position
-	is_on_screen = true
+	at_target_position = true
 
 
 func move_off_screen() -> void:
 	set_process_input(false)
 	move_to = off_screen_position
-	is_on_screen = false
+	at_target_position = false
 
 
 func switch_position() -> void:
-	if is_on_screen:
-		move_to = off_screen_position
+	if at_target_position:
+		move_off_screen()
 	else:
-		move_to = on_screen_position
-	is_on_screen = !is_on_screen
+		move_on_screen()
