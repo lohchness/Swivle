f2a75dc0587bffdcf7fa62690da2290390d87878
Loki Chow <lohchness@gmail.com>
2025-07-30 10:57:30 +1000

    refactor: gamesummary inherits twostateswitch instead of control

--- scripts/game_summary.gd
+++ scripts/game_summary.gd
@@ -1,14 +1,15 @@
 class_name GameSummary
-extends GridContainer
+extends TwoStateSwitch
 
 ## This class is meant to be contained in its own GameOver scene,
 ## separate from the Game scene. It pulls stats from GlobalContext
 ## to display in the labels.
 
-@onready var score: Label = $Score
-@onready var word: Label = $Word
+@onready var score: Label = $CenterContainer/GridContainer/Score
+@onready var words: Label = $CenterContainer/GridContainer/Words
 
 
 func _ready() -> void:
+	super()
 	score.text = str(Globals.score)
-	word.text = Globals.word
+	#words.text = Globals.word
